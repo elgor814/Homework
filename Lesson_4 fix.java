43d82c4390554bad79688924f315e3fbe9bdb767
elgor814 <168122154+elgor814@users.noreply.github.com>
2024-05-20 12:06:37 +0300

    Add files via upload

---
+++ Lesson_4 fix.java
@@ -0,0 +1,218 @@
+
+
+class Num1 {
+    static void printThreeWords(String fruit) {
+        System.out.println(fruit);
+    }
+
+    public static void main(String[] args) {
+        printThreeWords("Orange ");
+        printThreeWords("Banana");
+        printThreeWords("Apple");
+    }
+}
+
+class Num2 {
+    static void checkSumSign(int a, int b){
+        if (a+b >= 0){
+            System.out.println ( "Сумма положительная");
+        }
+        else
+            System.out.println ( "Сумма отрицательная");
+    }
+    public static void main (String[] args){
+        int f = 3;
+        int h =-9;
+        checkSumSign( f, h);
+    }
+}
+
+
+class Num3{
+    static void printColor(int value){
+        if  (value <= 0) {
+            System.out.print("Красный");
+        }
+        else if (value > 100) {
+            System.out.print("Зеленый");
+        }
+        else {
+            System.out.print("Желтый");
+        }
+    }
+    public static void main (String[] args){
+        int a = -5;
+        printColor(a);
+    }
+}
+
+
+class Num4{
+    static void  compareNumbers(int a, int b){
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else System.out.println("a < b");
+    }
+    public static void main (String[] args){
+        int z = 7;
+        int x = 25;
+        compareNumbers(z, x);
+    }
+}
+
+
+class Num5{
+    static boolean TrueFalse(int a, int b){
+        if (a + b < 10 )
+        {return false;}
+        else if (a + b > 20) {return false;}
+        else {return true;}
+
+    }
+    public static void main (String[] args){
+        int q = 6;
+        int w = 25;
+        TrueFalse(q, w);
+        //System.out.println(TrueFalse(q, w));
+    }
+}
+
+
+class Num6{
+    static void  pozNeg(int a){
+        if (a >= 0) System.out.println("Число положительное");
+
+        else System.out.println("Число отрицательное");
+    }
+    public static void main (String[] args){
+        int c = 0;
+        pozNeg(c);
+    }
+}
+
+
+class Num7 {
+    static boolean pozNeg(int a) {
+        if (a <= 0)
+            return true;
+        else
+            return false;
+    }
+    public static void main (String[] args){
+        int c = 7;
+        pozNeg(c);
+    }
+}
+
+
+class Num8{
+    static void myMethod(String str1, int a) {
+        for (int i = 1; i<=a; i++)
+            System.out.print(str1);
+    }
+    public static void main(String[] args) {
+        String str = "qwerty ";
+        int c = 5;
+        myMethod(str, c);
+    }
+}
+
+
+class Num9{
+    static boolean myMethod (int year){
+        if ((year % 4 ==0 || year % 100 == 0 )&& year%400 == 0 )
+            return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+        int year1 = 200;
+        myMethod (year1);
+    }
+}
+
+
+class Num10 {
+    public static void main(String[] args){
+        int[] array1 = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+        for(int i=0; i<=array1.length-1; i++) {
+            if (array1[i] ==0) {array1[i]=1;}
+            else array1[i] =0;
+            System.out.println(array1[i]);
+        }
+    }
+}
+
+
+class Num11 {
+    public static void main(String[] args){
+        int[] myArray = new int[100]; // Создаем массив длиной 5
+
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = i + 1; // Заполняем массив значениями от 1 до 5
+        }
+        System.out.print("Значения массива:");
+        System.out.print("[");
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print( myArray[i] +" " );
+        }
+        System.out.print("]");
+    }
+}
+
+
+class Num12 {
+    public static void main(String[] args) {
+        int[] array1 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] < 6) {
+                array1[i] *= 2;
+            }
+        }
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + " ");
+        }
+    }
+}
+
+
+class Num13 {
+    public static void main(String[] args) {
+
+        int size = 5;
+        int[][] squareArray = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j || i + j == squareArray.length - 1) {
+                    squareArray[i][j] = 1;
+                }
+                System.out.print(squareArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+class Num14 {
+    public static int[] metArr(int len, int initialValue) {
+        int[] newArr = new int[len];
+        for (int i = 0; i < len; i++) {
+            newArr[i] = initialValue;
+            //System.out.println(arr[i]);
+        }
+        return newArr;
+    }
+    public static void main(String[] args) {
+        int len = 5;
+        int initialValue = 10;
+        metArr(len, initialValue);
+    }
+}
+
+
+
+
+
