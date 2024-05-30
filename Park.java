e3e895873c362a8380b9d7aa8f638f045e35094a
elgor814 <168122154+elgor814@users.noreply.github.com>
2024-05-30 09:29:52 +0300

    Add files via upload

--- Park.java
+++ Park.java
@@ -1,18 +1,35 @@
 public class Park {
-    //public static void main (String[] args){
-        //Park attraction1 = new Attraction();
-    public class Attractions{   // внутренний класс аттракционы
-        String name;
-        String workTime;
-        int coast;
 
-       public Attraction(String name, String workTime, int coast); //конструктор аттракционов
-        this.String name = name;
-        this String workTime = workTime;
-        this coast = coast;
-        Park.Attractions attraction = Attractions.new Attractions()
+    public static class Attraction {   // внутренний класс аттракционы
+        private String name;
+        private String workTime;
+        private int price;
 
+        public Attraction(String name, String workTime, int price) { //конструктор аттракциона
+            this.name = name;
+            this.workTime = workTime;
+            this.price = price;
+        }
+
+        public Attraction() {
+        }
+
+        // Методы доступа к полям аттракционов
+        public String getName() {
+            return name;
+        }
+
+        public String getWorkTime() {
+            return workTime;
+        }
+
+        public int getCoast() {
+            return price;
+        }
 
     }
+
+    public static void main(String[] args) {
+        Park.Attraction attraction = new Attraction();
     }
 }
