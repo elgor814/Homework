a37331d9bda27256cbd0081aec23320c87928fa4
elgor814 <168122154+elgor814@users.noreply.github.com>
2024-05-31 13:02:56 +0300

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,10 +1,10 @@
-class Employee {
-    String name;
-    String jobTitle;
-    String eMail;
-    long phoneNum;
-    int salary;
-    int age;
+public class Employee {
+    private String name;
+    private String jobTitle;
+    private String eMail;
+    private long phoneNum;
+    private int salary;
+    private int age;
 
     public Employee(String name, String jobTitle, String eMail, long phoneNum, int salary, int age) {
         this.name = name;
@@ -18,24 +18,5 @@ class Employee {
     // метод для вывода информации о человеке
     public void displayInfo() {
         System.out.println("Сотрудник: " + name + " " + jobTitle + " " + eMail + " " + phoneNum + " " + salary + age);
-    }
-
-
-    public static void main(String[] args) {
-        // создание объекта
-        Employee Alex = new Employee("Alex", "tester", "alextest@gmail.com", 1234567890, 2000, 30);
-        Employee Alex1 = new Employee("Alex1", "tester", "alex1test@gmail.com", 1234567890, 3000, 50);
-        //вызов метода вывода
-        Alex1.displayInfo();
-        Alex.displayInfo();
-
-        Employee[] arrayEmployee = new Employee[5]; // объявляем массив объектов
-
-        arrayEmployee[0] = new Employee("Ivanov Ivan", "Инженер", " ivivan@mailbox.com ", 892312312, 30000, 30);
-        arrayEmployee[1] = new Employee("Semenov Semen", "Инженер", "semen@mailbox.com ", 811111111, 30000, 31);
-        arrayEmployee[2] = new Employee("Sergeev Sergei", "старший Инженер", " Sergeev@mailbox.com ", 822222222, 40000, 32);
-        arrayEmployee[3] = new Employee("Leonidov Leonid", "старший Инженер", " Alexeev@mailbox.com ", 833333333, 40000, 33);
-        arrayEmployee[4] = new Employee("Alekseev Aleksei", "ведущий Инженер", "leonid@mailbox.com ", 844444444, 30000, 45);
-        arrayEmployee[3].displayInfo(); // вывод в консоль информацию об объекте из массива
     }
 }
--- Main.java
+++ Main.java
@@ -6,6 +6,27 @@ public class Main {
     public static void main(String[] args) {
         fiveAttractions();
         addSixthAttration();
+        System.out.println ("                                             ");
+        System.out.println ("                                             ");
+        System.out.println (" Сотруднички:");
+
+        Employee alex = new Employee("Alex", "tester", "alextest@gmail.com", 1234567890, 2000, 30);
+        Employee alex1 = new Employee("Alex1", "tester", "alex1test@gmail.com", 1234567890, 3000, 50);
+        //вызов метода вывода
+        alex1.displayInfo();
+        alex.displayInfo();
+
+        Employee[] arrayEmployee = new Employee[5]; // объявляем массив объектов
+
+        arrayEmployee[0] = new Employee("Ivanov Ivan", "Инженер", " ivivan@mailbox.com ", 892312312, 30000, 30);
+        arrayEmployee[1] = new Employee("Semenov Semen", "Инженер", "semen@mailbox.com ", 811111111, 30000, 31);
+        arrayEmployee[2] = new Employee("Sergeev Sergei", "старший Инженер", " Sergeev@mailbox.com ", 822222222, 40000, 32);
+        arrayEmployee[3] = new Employee("Leonidov Leonid", "старший Инженер", " Alexeev@mailbox.com ", 833333333, 40000, 33);
+        arrayEmployee[4] = new Employee("Alekseev Aleksei", "ведущий Инженер", "leonid@mailbox.com ", 844444444, 30000, 45);
+        arrayEmployee[3].displayInfo(); // вывод в консоль информацию об объекте из массива
+
+
+
     }
 
     private static void fiveAttractions() {
