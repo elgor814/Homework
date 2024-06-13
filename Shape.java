5cb0b998da75c6aa17dabf6eee278fcf66d13c36
elgor814 <168122154+elgor814@users.noreply.github.com>
2024-06-13 18:00:46 +0300

    Delete Shape.java

--- Shape.java
+++
@@ -1,6 +0,0 @@
-interface Shape {
-    double calculatePerimeter(); // вычисление периметра
-    double calculateArea(); // вычисление площади
-    String getFillColor(); // цвет заливки
-    String getBorderColor(); // цвет границ
-}
